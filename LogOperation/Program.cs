3d8b29879d570efc8dd7a8d67d8ed47aab402bea
Arda ONUR <arda.onur@hotmail.com>
2021-01-30 19:16:46 +0200

    Add project files.

---
+++ LogOperation/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager=new CustomerManager(new FileLogger());
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Data logged to database");
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Data logged to sms");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Data logged to file");
+        }
+    }
+
+    class CustomerManager
+    {
+        private ILogger _logger;
+
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+
+        public void Add()
+        {
+            _logger.Log();
+            Console.WriteLine("Data logged!");
+        }
+    }
+}
